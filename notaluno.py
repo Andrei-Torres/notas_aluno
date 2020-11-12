5344f04707be9a0693dd4e0eb2b25761ce934c11
Andrei-Torres <andreisoarestorres@gmail.com>
2020-11-12 15:21:10 -0300

    Create notaluno.py
    exercício para prática da linguagem python, digitando a matricula e 3 notas para o aluno, respondera as médias e se aprovado ou reprovado.

---
+++ notaluno.py
@@ -0,0 +1,46 @@
+# funções declaradas
+def media (nota1, nota2, nota3):
+    return ((nota1 + nota2 + nota3)/3)
+
+def media_aproveitamento (nota1, nota2, nota3):
+    return((nota1 + nota2 * 2 + nota3 * 3 + media(nota1, nota2, nota3) )/7)
+
+def conceito (ma):
+    if (ma >= 9):
+        return "A"
+    elif (ma >=7.5):
+        return "B"
+    elif (ma >=6):
+        return "C"
+    elif (ma >=4):
+        return "D"
+    else:
+        return "E"
+        
+def mensagem (con):
+    if (con == "A" or con == "B" or con == "C"):
+        return "APROVADO"
+    else:
+        return "REPROVADO"
+
+# variaves de entrada
+aluno = int(input('Digite a matrícula do aluno: '))
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+nota3 = int(input('Digite a terceira nota: '))
+
+# variaveis chamando funções para facilitar na manipulação das menssagens
+ma = media_aproveitamento(nota1, nota2, nota3)
+con = conceito(ma)
+msg = mensagem(con)
+
+# dados de saída
+print('-------------------------------------')
+print('O Aluno:', aluno ,'obteve as notas:')
+print(nota1)
+print(nota2)
+print(nota3)
+print('Média: ', media(nota1, nota2, nota3))
+print('Média de Aproveiamento: ',ma)
+print('Conceito: ',con)
+print("Aluno ",msg)
